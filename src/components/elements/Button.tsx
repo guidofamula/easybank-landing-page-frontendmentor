f26af05e624a334bec00346b74c8385941ef6143
Guido Famula <guidofamula29@gmail.com>
2022-12-19 15:16:20 +0700

    edit elements

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -4,11 +4,11 @@ const Button = (props: any) => {
 		<button
 			className={`${classButton} bg-gradient-to-r from-primary-lime-green to-primary-bright-cyan rounded-3xl p-2 px-4 hover:opacity-70 transition duration-300 ease-in-out`}
 		>
-			<h1
+			<div
 				className={`${classH1} text-netral-white font-normal-custom text-[14px] px-2`}
 			>
 				{children}
-			</h1>
+			</div>
 		</button>
 	);
 };
